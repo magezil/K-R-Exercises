9c2b756a47f337ac6f6a0e72e31d18d161f87c4a
me <28716052+magezil@users.no-reply.github.com>
2017-07-24 18:55:32 -0700

    Exercise 1-5 with symbolic constants (and Celsius range back to 300-0 instead of 280-[-20])

--- chapter1/temp_conversion_rev.c
+++ chapter1/temp_conversion_rev.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
+/* Define constants */
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
 
   int fahr, celsius;
 
   printf("%s", "Fahrenheit-Celsius table\n");
-  for (fahr = 300; fahr >= 0; fahr -= 20)
+  for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
     printf("%3d %6.1f\n", fahr, 5.0 * (fahr-32.0) / 9.0);
 
   /* Celsius-Fahrenheit table */
   printf("%s", "Celsius-Fahrenheit table \n");
-  for (celsius = 280; celsius >= -20; celsius -= 20)
+  for (celsius = UPPER; celsius >= LOWER; celsius -= STEP)
     printf("%3d %6.1f\n", celsius, 9.0 / 5.0 * celsius + 32.0);
 }
