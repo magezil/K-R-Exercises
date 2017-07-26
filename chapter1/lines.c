08df7d740884e50b87e21698effa4e12a7a8b4d4
me <28716052+magezil@users.no-reply.github.com>
2017-07-26 16:59:50 -0700

    Fix 1-18 to remove trailing space. Create new method to replace excess space to single space

--- chapter1/lines.c
+++ chapter1/lines.c
@@ -4,13 +4,13 @@
 int mygetline(char line[], int maxline);
 void copy(char to[], char from[]);
 void reverse(char s[], char from[]);
+void despace(char s[]);
 
 /* Exercise 1-16
    print lines of size n or greater */
 int main()
 {
-  int len;
-  int n;
+  int len, n;
   char line[MAXLINE];
   char qualified[MAXLINE];
   char reversed[MAXLINE];
@@ -19,36 +19,41 @@ int main()
   n = 80;
   while ((len = mygetline(line, MAXLINE)) > 0) {
     if (len >= n) {
+      despace(line);
       copy(qualified, line);
       printf("%s", qualified);
       reverse(reversed, line);
       printf("%s\n", reversed);
     }
   }
+  
+  return 0;
 }
+
 
 int mygetline(char s[], int lim)
 {
   int c, i;
-  int blank = 0;
+  int nonblank = 0;
 
   // Modified for Exercise 1-18
   for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; i++){
-    if (c==' ' || c=='\t') {
-      s[i] = ' ';
-      while ((c=getchar())!=EOF && c==' ');
-      if (c!=EOF) {
-	i++;
-	s[i] = c;
-      }
-    }
-    else
-      s[i] = c;
-  }
-  if (c=='\n') {
+    if (!(c==' ' || c=='\t'))
+      nonblank = i;
     s[i] = c;
+  }
+  if (i > 0) {
+    if (i!=(nonblank+1))
+      i = nonblank+1;
+    if (c==EOF)
+      c='\n';
+  }
+
+  if (c=='\n'){
+    s[i] = '\n';
     i++;
   }
+
   s[i] = '\0';
   return i;
 }
@@ -66,11 +71,37 @@ void reverse(char rev[], char from[])
   // Assume line is not longer than MAXLINE
   int i, count;
 
-  for (count=0; from[count] != '\0'; count++);
+  for (count=0; (from[count] != '\n' && from[count] != '\0'); count++);
 
   for (i=0; i<count; i++)
     rev[i]=from[count-i-1];
 
   rev[i]='\n';
   rev[i+1]='\0';
+}
+
+/* remove extra spaces and tabs in middle of text - change to single space */
+void despace(char s[])
+{
+  int c, i, temp;
+
+  temp = 0;
+  for (i=0; (c=s[i]) != '\0'; i++) {
+    if (c==' ' || c=='\t') {
+      s[temp] = ' ';
+      while ((c=s[i])!=EOF && (c==' ' || c=='\t'))
+	i++;
+
+      if (c != EOF) {
+	temp++;
+	s[temp] = c;
+	temp++;
+      }
+    }
+    else {
+      s[temp] = c;
+      temp++;
+    }
+  }
+  s[temp]='\0';
 }
